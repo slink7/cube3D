5773857b1010f4e25defbf9d919b0788d4b3d1ea
Sebastien Cambier <slink794@gmail.com>
2024-05-09 17:49:23 +0200

    Fixed crc32 !

--- load_png.c
+++ load_png.c
@@ -6,7 +6,7 @@
 /*   By: scambier <scambier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 22:19:20 by scambier          #+#    #+#             */
-/*   Updated: 2024/05/09 17:00:47 by scambier         ###   ########.fr       */
+/*   Updated: 2024/05/09 17:48:45 by scambier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,9 +42,12 @@ unsigned int	crc32(unsigned char *bloc, unsigned int size)
 	return (~crc);
 }
 
-int	bytes_to_int(char *bytes)
+unsigned int	bytes_to_int(char *bytes)
 {
-	return ((bytes[0] << 24) + (bytes[1] << 16) + (bytes[2] << 8) + (bytes[3] << 0));
+	return ((0xFF000000 & bytes[0] << 24)
+		+ (0x00FF0000 & bytes[1] << 16)
+		+ (0x0000FF00 & bytes[2] << 8)
+		+ (0x000000FF & bytes[3] << 0));
 }
 
 int	read_int(int fd)
@@ -66,31 +69,19 @@ char	*read_malloc(int fd, int length)
 
 void	read_chunk(int fd)
 {
-	char	*length;
-	int		length_value;
-	char	*type;
+	int		length;
 	char	*data;
-	int		crc_value;
-	char	*concat;
+	unsigned int	crc;
 
-	int		crc1;
+	length = read_int(fd);
 
-	length = read_malloc(fd, 4);
-	length_value = bytes_to_int(length);
-	
-	type = read_malloc(fd, 4);
+	data = read_malloc(fd, 4 + length);
 
-	data = read_malloc(fd, length_value);
-	ft_pmem(data, length_value);
-
-	crc_value = read_int(fd);
-
-	char *a[4] = {(char *)type, (char *)data, (char *)length, 0};
-	concat = ft_strsjoin((char **)a, "");
-	ft_pmem(concat, ft_strlen(concat));
-	crc1 = crc32((unsigned char *)concat, ft_strlen(concat));
-	ft_printf("Read crc: 0x%x\n", crc_value);
-	ft_printf("Computed crc: 0x%x\n", crc1);
+	crc = read_int(fd);
+	if (crc != crc32(data, length + 4))
+	{
+		ft_printf("Error: CRC token is invalid\n");
+	}
 }
 
 t_image *load_png(char *path)
@@ -110,28 +101,6 @@ t_image *load_png(char *path)
 		ft_printf("Error: Invalid PNG signature");
 		return (0);
 	}
-	// read(fd, buffer, 25);
-	// ft_pmem(buffer, 25);
-	
-	// int crc1 = validation_crc(maj_crc(0xFFFFFFFF, (unsigned short *)buffer, 21));
-	// ft_printf("calc : %x\n", crc1);
-	
-	// char *str = "abc";
-	// ft_printf("crc32 de [%s] : 0x%x\n", str, crc32(str, ft_strlen(str)));
-	
 	read_chunk(fd);
-	
-	// read(fd, buffer, 4);
-	// ft_pmem(buffer, 4);
-	// read(fd, buffer, 4); //4 IHDR
-	// ft_pmem(buffer, 4); 
-	// if (ft_strncmp("IHDR", buffer, 4) != 0)
-	// {
-	// 	ft_printf("Error: Missing IHDR chunk");
-	// 	return (0);
-	// }
-	// out->width = read_int(fd); //4 WIDTH
-	// out->height = read_int(fd); //4 HEIGHT
-	// ft_printf("DIM : %d * %d\n", out->width, out->height);
-	return (0);
+	return (out);
 }
